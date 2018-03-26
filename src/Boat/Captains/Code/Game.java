67813f153d0c2603a64e584e0a14374cc6b13aa3
Captain <mads130f@edu.easj.dk>
2018-03-26 15:25:59 +0200

    yeah boii

--- src/Boat/Captains/Code/Game.java
+++ src/Boat/Captains/Code/Game.java
@@ -1,13 +1,24 @@
 package Boat.Captains.Code;
 
+import java.util.ArrayList;
+
 public class Game {
     public static void main(String[] args) {
 
 
 
         Cell[][] gamesArray = new Cell[10][10];
+        ArrayList<Cell> cellArrayList = new ArrayList<>();
 
-        GoThrough goThrough = new GoThrough(gamesArray);
+        //Creating the cells !!!HAS TO BE MOVED
+        gamesArray[5][5] = new Cell('a');
+        gamesArray[4][5] = new Cell('a');
+        gamesArray[4][4] = new Cell('a');
+        gamesArray[5][6] = new Cell('a');
+
+
+
+        GoThrough goThrough = new GoThrough(gamesArray,cellArrayList);
         goThrough.updateState();
 
 
--- src/Boat/Captains/Code/GoThrough.java
+++ src/Boat/Captains/Code/GoThrough.java
@@ -1,35 +1,51 @@
 package Boat.Captains.Code;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GoThrough {
 
     //En klasse der går igennem arrayen for at checke hvormange der er i live og derefter ændrer deres state
 
-    Cell[][] gamesArray;
+    private Cell[][] gamesArray;
 
     int lengthSide = GettableInfo.lengthSide;
     int lengthUpDown = GettableInfo.lengthSide;
 
     PrintInConsole printInConsole = new PrintInConsole();
 
-    GoThrough(Cell[][] gamesArray){
+    int counter=0;
+
+
+
+    GoThrough(Cell[][] gamesArray, ArrayList arrayList){
         this.gamesArray = gamesArray;
     }
 
+
+    void creation(){
+        //Sikrer sig at den kun bliver kørt en gang
+        if(this.counter<1){
+
+            //Fill array with dead cells
+            for (int i = 0; i < lengthSide-1 ; i++) {
+                for (int j = 0; j < lengthUpDown-1 ; j++) {
+                    gamesArray[i][j]=new Cell('d');
+                }
+            }
+            //Smider nogen nye Cells ind
+            gamesArray[5][5] = new Cell('a');
+            gamesArray[4][5] = new Cell('a');
+            gamesArray[4][4] = new Cell('a');
+            gamesArray[5][6] = new Cell('a');
+            this.counter++;
+        }else{
+            System.out.println("--Creation has been run before-- ");
+        }
+    }
     void updateState(){
 
-
-        //Fill array with dead cells
-        for (int i = 0; i < lengthSide-1 ; i++) {
-            for (int j = 0; j < lengthUpDown-1 ; j++) {
-                gamesArray[i][j]=new Cell('d');
-            }
-        }
-
-        gamesArray[5][5] = new Cell('a');
-        gamesArray[4][5] = new Cell('a');
-        gamesArray[4][4] = new Cell('a');
-        gamesArray[5][6] = new Cell('a');
-
+        creation();
 
         //Printer i console
         printInConsole.printerino(gamesArray);
@@ -39,22 +55,25 @@ public class GoThrough {
         for (int i = 0; i < (lengthSide-1) ; i++) {
             for (int j = 0; j < (lengthUpDown-1) ; j++) {
                 if(gamesArray[i][j].isAlive()) {
-                    System.out.println("found cell at "+ i +" : "+j);
-                    //bundrækken (iforhold til)
-                    gamesArray[i+1][j+1].setLivingNeighbours(+1);
-                    gamesArray[i][j+1].setLivingNeighbours(+1);
-                    gamesArray[i-1][j+1].setLivingNeighbours(+1);
-                    //Midter rækken (iforhold til)
-                    gamesArray[i+1][j].setLivingNeighbours(+1);
-                    gamesArray[i-1][j].setLivingNeighbours(+1);
-                    //toprækken (iforhold til)
-                    gamesArray[i+1][j-1].setLivingNeighbours(+1);
-                    gamesArray[i][j-1].setLivingNeighbours(+1);
-                    gamesArray[i-1][j-1].setLivingNeighbours(+1);
+                    try {
+                        System.out.println("found cell at " + i + " : " + j);
+                        //bundrækken (iforhold til)
+                        gamesArray[i + 1][j + 1].setLivingNeighbours(+1);
+                        gamesArray[i][j + 1].setLivingNeighbours(+1);
+                        gamesArray[i - 1][j + 1].setLivingNeighbours(+1);
+                        //Midter rækken (iforhold til)
+                        gamesArray[i + 1][j].setLivingNeighbours(+1);
+                        gamesArray[i - 1][j].setLivingNeighbours(+1);
+                        //toprækken (iforhold til)
+                        gamesArray[i + 1][j - 1].setLivingNeighbours(+1);
+                        gamesArray[i][j - 1].setLivingNeighbours(+1);
+                        gamesArray[i - 1][j - 1].setLivingNeighbours(+1);
 
-                    System.out.println("Cell at "+ i +" : "+j);
-                    System.out.println(gamesArray[5][4].getLivingNeighbours());
-
+                        System.out.println("Cell at " + i + " : " + j);
+                        System.out.println(gamesArray[5][4].getLivingNeighbours());
+                    }catch (ArrayIndexOutOfBoundsException e){
+                        e.printStackTrace();
+                    }
                 }
 
             }
@@ -66,10 +85,15 @@ public class GoThrough {
 
             }
         }
+
         //Printer igen
         printInConsole.printerino(gamesArray);
 
-
+        Scanner scanner = new Scanner(System.in);
+        String helloworld = scanner.next();
+        if(helloworld.equals("0")){
+            updateState();
+        }
     }
 
 
